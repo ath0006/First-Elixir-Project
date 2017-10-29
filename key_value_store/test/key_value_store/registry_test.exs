f6a203c51c431d0c0fa97e4aab26d09e8dd0e20f
unknown <ahuggins0006@gmail.com>
2017-10-29 14:46:13 -0500

    Created tests for registry.ex and updated registry.ex to handle a process going down or being unresponsive

---
+++ key_value_store/test/key_value_store/registry_test.exs
@@ -0,0 +1,25 @@
+defmodule KEY_VALUE_STORE.RegistryTest do
+    use ExUnit.Case, async: true
+
+    setup do
+      {:ok, registry} = start_supervised KEY_VALUE_STORE.Registry
+      %{registry: registry}
+    end
+
+    test "spawns buckets", %{registry: registry} do
+      assert KEY_VALUE_STORE.Registry.lookup(registry, "shopping") == :error
+
+      KEY_VALUE_STORE.Registry.create(registry, "shopping")
+      assert {:ok, bucket} = KEY_VALUE_STORE.Registry.lookup(registry, "shopping")
+
+      KEY_VALUE_STORE.Bucket.put(bucket, "milk", 1)
+      assert KEY_VALUE_STORE.Bucket.get(bucket, "milk") == 1
+    end
+
+    test "removes buckets on exit", %{registry: registry} do
+      KEY_VALUE_STORE.Registry.create(registry, "shopping")
+      {:ok, bucket} = KEY_VALUE_STORE.Registry.lookup(registry, "shopping")
+      Agent.stop(bucket)
+      assert KEY_VALUE_STORE.Registry.lookup(registry, "shopping") == :error
+    end
+end
