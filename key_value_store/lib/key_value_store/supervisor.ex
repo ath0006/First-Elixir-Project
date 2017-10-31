facc45b59ee66bb5a2f7ee73efc7f29a128bbe9b
unknown <ahuggins0006@gmail.com>
2017-10-30 21:35:55 -0500

    updated the supervisor to create a tuple for the  init function

--- key_value_store/lib/key_value_store/supervisor.ex
+++ key_value_store/lib/key_value_store/supervisor.ex
@@ -1,13 +1,13 @@
 defmodule KEY_VALUE_STORE.Supervisor do
     use Supervisor
 
-    def start_links(opts) do
+    def start_link(opts) do
       Supervisor.start_link(__MODULE__, :ok, opts)
     end
 
     def init(:ok) do
       children = [
-          KEY_VALUE_STORE.Registry
+          {KEY_VALUE_STORE.Registry, name: KEY_VALUE_STORE.Registry}
       ]
 
       Supervisor.init(children, strategy: :one_for_one)
